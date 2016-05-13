f2a38f4442586632ddeb27aba452d49ee57a13c7
Alex Schultz <aschultz@mirantis.com>
2016-05-13 15:45:38 -0600

    basic tests for flanneld

--- deployment_scripts/puppet/modules/flannel/spec/classes/init_spec.rb
+++ deployment_scripts/puppet/modules/flannel/spec/classes/init_spec.rb
@@ -3,5 +3,21 @@ describe 'flannel' do
 
   context 'with default values for all parameters' do
     it { should contain_class('flannel') }
+
+    it 'should install flannel package' do
+     is_expected.to contain_package('flannel')
+    end
+
+    it 'should configure upstart' do
+      is_expected.to contain_file('/etc/init/flanneld.conf')
+    end
+
+    it 'should configure flannel opts' do
+      is_expected.to contain_file('/etc/default/flanneld')
+    end
+
+    it 'should start flanneld service' do
+      is_expected.to contain_service('flanneld')
+    end
   end
 end
